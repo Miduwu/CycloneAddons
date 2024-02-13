9ead3a128c8c8feb5732356a40bb3e2b954c1030
zoptu <86263102+zopsitokun@users.noreply.github.com>
2024-02-12 22:23:36 -0600

    Update Nsfw.js

--- cmds/Interactions/Nsfw.js
+++ cmds/Interactions/Nsfw.js
@@ -7,12 +7,18 @@ type: Interactions.Button
 }),
 async code(interaction) {
   let nsfwRole = interaction.guild.roles.cache.get("917661809455034428") || await interaction.guild.roles.fetch("917661809455034428")
-  if(!interaction.member.roles.cache.has(nsfwRole)) {
+  if(!interaction.member.roles.cache.has(nsfwRoles)) {
     await interaction.member.roles.add(nsfwRole)
-    await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_plus:1057545930443870208> | ***Rol añadido a tu lista.***").setColor(Auxiliar.Colors.cyan)], ephemeral: true})
+    let embed = new EmbedBuilder()
+      .setDescription("<:cyaddons_plus:1057545930443870208> | ***Rol añadido a tu lista.***")
+      .setColor(Auxiliar.Colors.cyan)
   } else {
     await interaction.member.roles.remove(nsfwRole)
-    await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_minus:1057546000622964746> | ***Rol removido de tu lista.***").setColor(Auxiliar.Colors.red)], ephemeral: true})
+    let embed = new EmbedBuilder()
+      .setDescription("<:cyaddons_minus:1057546000622964746> | ***Rol removido de tu lista.***")
+      .setColor(Auxiliar.Colors.red)
   }
+  return interaction.reply({embeds: [embed], ephemeral: true})
  }
-}
+  }
+  
