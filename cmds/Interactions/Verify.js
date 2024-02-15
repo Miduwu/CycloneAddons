e80aac4f4565722d695061910456f4857381472c
zoptu <86263102+zopsitokun@users.noreply.github.com>
2024-02-15 16:57:09 -0600

    Update Verify.js

--- cmds/Interactions/Verify.js
+++ cmds/Interactions/Verify.js
@@ -7,8 +7,8 @@ type: Interactions.Button
 }),
 async code(interaction) {
 let verifyrole = interaction.guild.roles.cache.get("917164283677278279") || await interaction.guild.roles.fetch("917164283677278279")
-if(!interaction.member.roles.cache.has(verifyrole)) {
-await interaction.member.roles.add(verifyrole)
+if(!interaction.member.roles.cache.has(verifyrole.id)) {
+await interaction.member.roles.add(verifyrole.id)
 await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_check:1060662306507333753> | ***Te verificaste exitosamente.***").setColor(Auxiliar.Colors.cyan)], ephemeral: true})
   } else {
 await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_error:1060665620468863096> | ***No es necesario, ya te verificaste.***").setColor(Auxiliar.Colors.red)], ephemeral: true})
