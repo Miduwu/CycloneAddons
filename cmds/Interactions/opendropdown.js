b12a218a762b913e8114cf7a3949d9001eaccbe2
zoptu <86263102+zopsitokun@users.noreply.github.com>
2024-02-18 10:14:53 -0600

    Update opendropdown.js

--- cmds/Interactions/opendropdown.js
+++ cmds/Interactions/opendropdown.js
@@ -7,7 +7,7 @@ name: "genders",
 type: Interactions.Button
 }),
 async code(interaction) {
-        if(interaction.member.roles.cache.has(roles.Famale || roles.Male || roles.Neutral)) return await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_dbwarning:1208648325814030427> | ***`Ya posees un rol de género.`***").setColor("FF46DA")], components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Remover Mi Rol Actual").setCustomId("deletegender").setStyle(2).setEmoji("<:cyaddons_dberror:1208648660779270164>"))], ephemeral: true})
+        if(interaction.member.roles.cache.has(roles.Famale || roles.Male || roles.Neutral)) return await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_dbwarning:1208648325814030427> | ***`Ya posees un rol de género.`***").setColor("FF46DA")], components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Remover").setCustomId("deletegender").setStyle(2).setEmoji("<:cyaddons_dberror:1208648660779270164>"))], ephemeral: true})
         let row = new ActionRowBuilder()
             .addComponents(new StringSelectMenuBuilder()
                 .setCustomId("ID_DROPDOWNS")
@@ -74,7 +74,7 @@ name: "deletegender",
 type: Interactions.Button
 }),
 async code(interaction) {
+await interaction.update({embeds: [new EmbedBuilder().setDescription("<:cyaddons_minus:1057546000622964746> | ***`Rol removido de tu lista.`***").setColor(Auxiliar.Colors.red)], components: []})
 await interaction.member.roles.remove(roles.Female || roles.Male || roles.Neutral)
-await interaction.update({embeds: [new EmbedBuilder().setDescription("<:cyaddons_minus:1057546000622964746> | ***`Rol removido de tu lista.`***").setColor(Auxiliar.Colors.red)]})
 }
 }]
