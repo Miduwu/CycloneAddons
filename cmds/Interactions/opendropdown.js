7a7fe58409296c9b8b436d89f85ae7658750e28c
zoptu <86263102+zopsitokun@users.noreply.github.com>
2024-02-17 21:45:22 -0600

    Update opendropdown.js

--- cmds/Interactions/opendropdown.js
+++ cmds/Interactions/opendropdown.js
@@ -1,16 +1,12 @@
 const { Interactions, ActionRowBuilder, ButtonBuilder, StringSelectMenuOptionBuilder, StringSelectMenuBuilder, InteractionBuilder, EmbedBuilder } = require("erine")
 const { Auxiliar } = require("../../index")
+let roles = { Female: "917435110591189052", Male: "917435094409564230", Neutral: "917435095172911135", Revive: "1208617632237101137", Anuncios: "917659645382901810", Alianzas: "919702909346803843" }
 module.exports["data"] = [{
 data: new InteractionBuilder({
 name: "genders",
 type: Interactions.Button
 }),
 async code(interaction) {
-  let roles = {
-Female: "917435110591189052",
-Male: "917435094409564230",
-Neutral: "917435095172911135"
-}
         let row = new ActionRowBuilder()
             .addComponents(new StringSelectMenuBuilder()
                 .setCustomId("ID_DROPDOWNS")
@@ -22,7 +18,7 @@ let embed = new EmbedBuilder()
 .setTitle("<:cyaddons_dbgenders:1208608303819661332> | Géneros")
 .setDescription("<:cyaddons_dbinfo:1207745246331666442> | ***`Selecciona tu género para evitar confusiones.`***")
 .setColor(Auxiliar.Colors.pink)
- await interaction.reply({embeds: [embed], components: [row]})
+ await interaction.reply({embeds: [embed], components: [row], ephemeral: true})
  }
   },{
 data: new InteractionBuilder({
@@ -30,11 +26,16 @@ name: "pings",
 type: Interactions.Button
 }),
 async code(interaction) {
-  let nsfwRole = interaction.guild.roles.cache.get("917661809455034428") || await interaction.guild.roles.fetch("917661809455034428")
-  if(!interaction.member.roles.cache.has(nsfwRole)) {
-    await interaction.member.roles.add(nsfwRole)
-    await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_plus:1057545930443870208> | ***Rol añadido a tu lista.***").setColor(Auxiliar.Colors.cyan)], ephemeral: true})
-  }
-}
-
+        let row = new ActionRowBuilder()
+            .addComponents(new StringSelectMenuBuilder()
+                .setCustomId("ID_DROPDOWNS")
+                .setPlaceholder("Selecciona Una Mención")
+      .addOptions(new StringSelectMenuOptionBuilder().setLabel("Anuncios").setValue(roles.Anuncios).setEmoji("<:cyaddons_dbannouncement:1208616564614635570>"),
+new StringSelectMenuOptionBuilder().setLabel("Alianzas").setValue(roles.Alianzas).setEmoji("<:cyaddons_dballiance:1208616603411939379>"),
+new StringSelectMenuOptionBuilder().setLabel("Revivir Chat").setValue(roles.Revive).setEmoji("<:cyaddons_dbmessage:1208616584919253053>")))
+let embed = new EmbedBuilder()
+.setTitle("<:cyaddons_dbping:1208616544612126720> | Menciones")
+.setDescription("<:cyaddons_dbinfo:1207745246331666442> | ***`Selecciona una mención para evitar el uso de @everyone.`***")
+.setColor(Auxiliar.Colors.pink)
+ await interaction.reply({embeds: [embed], components: [row], ephemeral: true})
 }]
