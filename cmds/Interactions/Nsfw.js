2d404dd728455e668934a5e34717f6fe05b6cc4c
zoptu <86263102+zopsitokun@users.noreply.github.com>
2024-02-18 08:45:57 -0600

    Update Nsfw.js

--- cmds/Interactions/Nsfw.js
+++ cmds/Interactions/Nsfw.js
@@ -9,7 +9,7 @@ async code(interaction) {
   let nsfwRole = interaction.guild.roles.cache.get("917661809455034428") || await interaction.guild.roles.fetch("917661809455034428")
   if(!interaction.member.roles.cache.has(nsfwRole)) {
     await interaction.member.roles.add(nsfwRole)
-    await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_plus:1057545930443870208> | ***Rol añadido a tu lista.***").setColor(Auxiliar.Colors.cyan)], ephemeral: true})
+    await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_nsfw:1206059710382608394> | ***Rol añadido a tu lista.***").setColor(Auxiliar.Colors.red)], ephemeral: true})
   } else {
     await interaction.member.roles.remove(nsfwRole)
     await interaction.reply({embeds: [new EmbedBuilder().setDescription("<:cyaddons_minus:1057546000622964746> | ***Rol removido de tu lista.***").setColor(Auxiliar.Colors.red)], ephemeral: true})
