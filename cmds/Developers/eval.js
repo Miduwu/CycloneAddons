604db23082ac527938f5fcfdc9b2abdf9c4df022
zoptu <86263102+zopsitokun@users.noreply.github.com>
2024-03-08 18:52:29 -0600

    Update eval.js

--- cmds/Developers/eval.js
+++ cmds/Developers/eval.js
@@ -1,28 +1,43 @@
 const { CommandBuilder, Plugins, Context, ParamsBuilder, AttachmentBuilder } = require("erine");
+const { inspect } = require("util");
 
 /**
  * @param {String} text 
  */
+const CleanCodeblockCode = (text) => {
+    if(text.startsWith("codeblock symbols") && text.endsWith("cc")) {
+        let lines = text.split("\n")
+        return lines.slice(1, lines.length - 1).join("\n")
+    } else return text;
+}
 
 module.exports["data"] = {
     data:  new CommandBuilder({
         name: "eval",
         aliases: ["try"],
-        description:  "Evalúa un código de JavaScript."
+        description:  "Evalúa un código de JavaScript.",
     }),
     plugins: [Plugins.isOwner],
     params: new ParamsBuilder()
-    .addString({ name: "código", description: "El código a evaluar", required: true, ellipsis: true }),
+    .addString({ name: "code", description: "El código a evaluar.", required: true, ellipsis: true }),
     /**
      * @param {Context} ctx
      */
     async code(ctx) {
-        let c = ctx.get("código")
         try {
-        let evaled = eval(c);
-        ctx.send(evaled.toString())
-        } catch(error) {
-            ctx.send(error.toString().slice(0, 1000))
+            let result = await eval(CleanCodeblockCode(ctx.args.get("code"))),
+            clean = inspect(result, { depth: 2 });
+            if(clean.length >= 1975) {
+                let file = new AttachmentBuilder(Buffer.from(clean)).setName("eval.txt")
+                ctx.send({ files: [file], content: ctx.author.toString() })
+            } else {
+                let formatted = "cosas de codeblocks aqui"
+                ctx.send({ content: `${ctx.author}\n${formatted}` })
+            }
+        } catch(err) {
+            let error = inspect(err, { depth: 2 })
+            let formattedError = `**ERROR** ...`
+            await ctx.send({ content:  `${ctx.author}\n${formattedError}`})
         }
     }
 }
