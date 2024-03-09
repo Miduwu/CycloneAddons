8f023268511e3f2df159bf56095ac71bbc27cd2a
zoptu <86263102+zopsitokun@users.noreply.github.com>
2024-03-09 17:40:52 -0600

    Update eval.js

--- cmds/Developers/eval.js
+++ cmds/Developers/eval.js
@@ -1,15 +1,4 @@
 const { CommandBuilder, Plugins, Context, ParamsBuilder, AttachmentBuilder } = require("erine");
-const { inspect } = require("util");
-
-/**
- * @param {String} text 
- */
-const CleanCodeblockCode = (text) => {
-    if(text.startsWith("codeblock symbols") && text.endsWith("cc")) {
-        let lines = text.split("\n")
-        return lines.slice(1, lines.length - 1).join("\n")
-    } else return text;
-}
 
 module.exports["data"] = {
     data:  new CommandBuilder({
@@ -19,25 +8,17 @@ module.exports["data"] = {
     }),
     plugins: [Plugins.isOwner],
     params: new ParamsBuilder()
-    .addString({ name: "code", description: "El código a evaluar.", required: true, ellipsis: true }),
+    .addString({ name: "código", description: "El código a evaluar.", required: true, ellipsis: true }),
     /**
      * @param {Context} ctx
      */
     async code(ctx) {
+        let evaled = eval(ctx.get("código"))
         try {
-            let result = await eval(CleanCodeblockCode(ctx.args.get("code"))),
-            clean = inspect(result, { depth: 2 });
-            if(clean.length >= 1975) {
-                let file = new AttachmentBuilder(Buffer.from(clean)).setName("eval.txt")
-                ctx.send({ files: [file], content: ctx.author.toString() })
-            } else {
-                let formatted = "cosas de codeblocks aqui"
-                ctx.send({ content: `${ctx.author}\n${formatted}` })
-            }
+            ctx.send(evaled)
         } catch(err) {
-            let error = inspect(err, { depth: 2 })
-            let formattedError = `**ERROR** ...`
-            await ctx.send({ content:  `${ctx.author}\n${formattedError}`})
+            ctx.send(err)
+            console.log(err)
         }
     }
 }
