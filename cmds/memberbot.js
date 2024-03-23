42581c3cc784f59b10088b5e25cba0d6b5ffe3fb
zoptu <86263102+zopsitokun@users.noreply.github.com>
2024-03-22 23:21:09 -0600

    Update memberbot.js

--- cmds/memberbot.js
+++ cmds/memberbot.js
@@ -1,5 +1,5 @@
 const { CommandBuilder, ParamsBuilder, EmbedBuilder, Context, ActionRowBuilder, ButtonBuilder } = require("erine");
-const { Database } = requiew("midb")
+const { Database } = require("midb")
 const db = new Database();
 const { Auxiliar } = require("../../index")
 module.exports["data"] = {
@@ -13,7 +13,7 @@ module.exports["data"] = {
       let mbot = ctx.bot.users.cache.get(ctx.get("bot").user.?id) || await ctx.bot.users.fetch(ctx.get("bot").user.?id)
       let mowner = ctx.bot.users.cache.get(await db.get(`owner_${mbot.id}`)) || await ctx.bot.users.fetch(await db.get(`owner_${mbot.id}`))
    if(!mbot.bot) return await ctx.send({embeds: [new EmbedBuilder().setDescription("| ***`El bot no existe.`***").setColor(Auxiliar.Colors.red)]}) 
-  if(!await db.has(`bot_${mbot.id}`)) return ctx.send({embeds: [new EmbedBuilder().setDescription("| ***`El bot está enlistado.`***").setColor(Auxiliar.Colors.red)]})
+  if(!await db.has(`bot_${mbot.id}`)) return ctx.send({embeds: [new EmbedBuilder().setDescription("| ***`El bot no está enlistado.`***").setColor(Auxiliar.Colors.red)]})
   let embed = new EmbedBuilder()
       .setAuthor({name: mbot.username, iconURL: mbot.displayAvatarURL({size: 4096}))
       .setTitle("<:cyaddons_dbidle:1220959205372989460> | Pendiente")
