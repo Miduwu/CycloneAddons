b2d008a6030292ba7f92a500afb22303015ed28e
zoptu <86263102+zopsitokun@users.noreply.github.com>
2024-03-24 22:15:23 -0600

    Create Acceptdeclinemodal.js

---
+++ cmds/Events/Acceptdeclinemodal.js
@@ -0,0 +1,21 @@
+const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require("erine")
+const event = {
+  name: "interactionCreate",
+  async code(i) {
+    if(!i.isButton()) return;
+    if(i.customId?.split("_")[0] == "accept") {
+       let botid = i.customId?.split("_")[1]
+        const commentinput = new TextInputBuilder()
+        .setCustomId("comments")
+        .setLabel("Comentarios")
+        .setPlaceholder("Deja cualquier comentario extra acerca del bot.")
+        .setRequired(false)
+        .setStyle(TextInputStyle.Paragraph)
+        const modal = new ModalBuilder()
+        .setTitle("Aceptar Bot")
+        .setCustomId(`ACCEPT_${botid}`)
+        .addComponents(new ActionRowBuilder().addComponents(commentinput))
+        i.showModal(modal).catch(console.log)
+    }
+})
+module.exports = { data: event }
